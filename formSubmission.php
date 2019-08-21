65eaebd0c6dab2969c28da0d477a66ac4899b6f7
ZeeshanAhmedoff <42231155+ZeeshanAhmedoff@users.noreply.github.com>
2019-08-21 17:19:17 +0500

    Add files via upload

---
+++ formSubmission.php
@@ -0,0 +1,5 @@
+<?php
+if(isset($_POST['passingYear'],$_POST['degTitle'],$_POST['eduLevel']))
+{
+    print_r($_POST);
+}
---
+++ main.js
@@ -0,0 +1,10 @@
+$("form").on('submit',function(){
+    var that= $(this),
+    
+    dataRow=
+    url= that.attr('action'),
+    method= that.attr('method'),
+    data={}
+    // console.log("trigger");
+    // return false;
+})
