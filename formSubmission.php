8423ab7184f01c8674892f53e1057affed24f8c8
ZeeshanAhmedoff <42231155+ZeeshanAhmedoff@users.noreply.github.com>
2019-08-23 14:25:03 +0500

    Add files via upload

--- formSubmission.php
+++ formSubmission.php
@@ -1,5 +1,25 @@
 <?php
-if(isset($_POST['passingYear'],$_POST['degTitle'],$_POST['eduLevel']))
-{
-    print_r($_POST);
-}
+    $degTitile=$_POST["degTitle"];
+    $passingYear=$_POST["PassingYear"];
+    $eduLevel= $_POST["eduLevel"];
+    $result= $_POST["res"];
+    $host="localhost";
+    $dbusername="root";
+    $dbpassword ="password";
+    $dbname="Users";
+    echo('hello');
+    $conn= new mysqli($host , $dbusername, $dbpassword,$dbname);
+    if(mysqli_connect_error()){
+        die('Connect Error('.mysqli_connect_errorno().')'.mysqli_connect_error());
+    }
+    else{
+        $sql="INSERT INTO user_Details(degTitle,passingYear,eduLevel,result) values ('$degTitile','$passingYear','$eduLevel','$result')";
+        if ($conn->query($sql)){
+            echo "New record is inserted successfully";
+        }
+        else{
+            echo "Error:".$sql."<br>".$conn->error;
+        }
+        $conn->close();
+    }
+?>
--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-$("form").on('submit',function(){
-    var that= $(this),
+$("#f").on('submit',function(){
+    var form=$(this);
+    data={};
+    // form.find('[name]').each(function(index,value){
+    //     console.log(value);
+    //      var field=$(this);
+    //      var name=field.attr("name");
+    //     var value=field.val()
+    //      data[name]=value;
+    // })
+    console.log('data object is',data);
+    $.ajax({
+        type: "POST",  
+        url: "formSubmission.php", 
+        data: form.serialize(),
+        success: function(response) {
+            console.log(response);
+            debugger;
+        }
+    });
     
-    dataRow=
-    url= that.attr('action'),
-    method= that.attr('method'),
-    data={}
-    // console.log("trigger");
-    // return false;
-})
+    return false;
+});
